ceabdd074276b679b410f5640309c1bedb802f75
Amy Ann Debono <56063946+amyanndebono@users.noreply.github.com>
2022-04-07 17:50:53 +0200

    Added menu and malta info.

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 from bs4 import BeautifulSoup
 import requests
 
-italyURL ='https://www.salute.gov.it/portale/nuovocoronavirus/dettaglioContenutiNuovoCoronavirus.jsp?lingua=english&id=5412&area=nuovoCoronavirus&menu=vuoto'
-result = requests.get(italyURL)
-doc = BeautifulSoup(result.text, "html.parser")
+def Malta(): 
+  MaltaUrl = "https://www.gov.uk/foreign-travel-advice/malta/entry-requirements"
+  result = requests.get(MaltaUrl)
+  doc = BeautifulSoup(result.text, "html.parser")
 
-#Attempting to access paragraph by first finding the word and the retrieving the parent. 
-#rules =  doc.find_all(text="rules")
+  para = doc.find_all("p")
 
-#Attempting to pass through the paragraph diectly 
-#para = doc.find('p').find_next('The rules')
-para = doc.find_all("p")
-
-for text in para:
-  print(text.get_text())
-
-#print(para)
+  for text in para: 
+    print(text.get_text())
 
 
-#Attempting to access p from div 
+def italy(): 
+  italyURL ='https://www.salute.gov.it/portale/nuovocoronavirus/dettaglioContenutiNuovoCoronavirus.jsp?lingua=english&id=5412&area=nuovoCoronavirus&menu=vuoto'
+  result = requests.get(italyURL)
+  doc = BeautifulSoup(result.text, "html.parser")
 
-#allDiv = doc.find_all('p')
-#for x in allDiv: 
-  #allPara = x.find('p')#.find_next(text ='rules')
-  #print(x)
- # rules = x.find_all(text = "rules")
-  
-#print(rules)
+  para = doc.find_all("p")
+
+  for text in para:
+    print(text.get_text())
+
+print("Select 1 for Malta")
+print("Select 2 for Italy")
+
+choice =input()
+
+if(choice == '1' ):
+  Malta()
+
+elif (choice == '2'):
+  italy()
+
+else: 
+  print("Invalid choice")
--- test.py
+++ test.py
@@ -38,3 +38,32 @@ doc = BeautifulSoup(result.text , "html.parser" )
 
 #with open("index.html", "w") as html_file:
  #   html_file.write(html_content)
+
+
+
+italyURL ='https://www.salute.gov.it/portale/nuovocoronavirus/dettaglioContenutiNuovoCoronavirus.jsp?lingua=english&id=5412&area=nuovoCoronavirus&menu=vuoto'
+result = requests.get(italyURL)
+doc = BeautifulSoup(result.text, "html.parser")
+
+#Attempting to access paragraph by first finding the word and the retrieving the parent. 
+#rules =  doc.find_all(text="rules")
+
+#Attempting to pass through the paragraph diectly 
+#para = doc.find('p').find_next('The rules')
+para = doc.find_all("p")
+
+for text in para:
+  print(text.get_text())
+
+#print(para)
+
+
+#Attempting to access p from div 
+
+#allDiv = doc.find_all('p')
+#for x in allDiv: 
+  #allPara = x.find('p')#.find_next(text ='rules')
+  #print(x)
+ # rules = x.find_all(text = "rules")
+  
+#print(rules)
