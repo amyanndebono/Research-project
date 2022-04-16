7a10da1ae8e0afba234eb90f1c4ad60a249d2c48
Amy Ann Debono <56063946+amyanndebono@users.noreply.github.com>
2022-04-16 17:55:09 +0200

    Info being displayed & scroll bar added.

--- app.py
+++ app.py
@@ -16,8 +16,11 @@ clicked = StringVar()
 clicked.set(options[0])
 
 drop = OptionMenu(root, clicked, *options )
-
 drop.pack()
 
 showButton = Button(root, text="Show Covid Regulations", command=show).pack()
+
+my_text = Text(root, width=60, height=20, font=("calibri", 12))
+my_text.pack(pady = 20)
+
 root.mainloop()
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 
 from bs4 import BeautifulSoup
+from pip import main
 import requests
-
+from tkinter import * 
+from matplotlib.pyplot import show
+from tkinter import ttk 
 
 germanyUrl = 'https://www.auswaertiges-amt.de/en/visa-service/EinreiseUndAufenthalt'
 MaltaUrl = "https://www.gov.uk/foreign-travel-advice/malta/entry-requirements"
@@ -14,55 +17,89 @@ portugalURL = 'https://www.visitportugal.com/en/content/covid-19-measures-implem
 switzerlandUrl = 'https://www.swissinfo.ch/eng/covid-19_coronavirus--the-situation-in-switzerland/45592192'
 polandURL = 'https://www.swissinfo.ch/eng/covid-19_coronavirus--the-situation-in-switzerland/45592192'
 
+root = Tk()
+root.title('Covid regulations')
+root.geometry("700x700")
+
+main_frame = Frame(root)
+main_frame.pack(fill=BOTH, expand=1)
+
+my_canvas = Canvas(main_frame)
+my_canvas.pack(side=LEFT, fill=BOTH , expand=1)
+
+scroll_barY = ttk.Scrollbar(main_frame, orient=VERTICAL, command=my_canvas.yview)
+#scroll_barX = ttk.Scrollbar(main_frame, orient=HORIZONTAL, command=my_canvas.xview)
+scroll_barY.pack(side=RIGHT, fill=Y)
+# scroll_barX.pack(side=RIGHT, fill=X)
+
+
+my_canvas.configure(yscrollcommand=scroll_barY)
+#my_canvas.configure(xscrollcommand=scroll_barX)
+
+my_canvas.bind('<Configure>', lambda e: my_canvas.configure(scrollregion= my_canvas.bbox("all")))
+
+second_frame = Frame(my_canvas)
+
+my_canvas.create_window((0,0), window=second_frame, anchor="nw")
+myLabel =Label(second_frame, text="").pack()
+
+
 def scrape(url):
   result = requests.get(url)
   doc = BeautifulSoup(result.text, "html.parser")
   para = doc.find_all("p")
-
+  
   for text in para: 
-    print(text.get_text())
+   myLabel =Label(second_frame, text=text.get_text()).pack()
 
-print("Select 1 for Malta")
-print("Select 2 for Italy")
-print("Select 3 for France")
-print("Select 4 for Austria")
-print("Select 5 for Germany")
-print("Select 6 for UK")
-print("7- spain")
-print ("8-portugal")
-print ("9-switzerland ")
-print("10-poland")
 
-choice =input()
-
-if(choice == '1' ):
-  scrape(MaltaUrl)
-
-elif (choice == '2'):
+def show():
+  if(clicked.get() == 'Malta' ):
+    myLabel =Label(second_frame, text="").pack()
+    scrape(MaltaUrl)
+  
+  elif (clicked.get() == 'Italy'):
+   myLabel =Label(second_frame, text="").pack()
    scrape(italyURL)
 
-elif(choice == '3'): 
-  scrape(FranceURL)
 
-elif(choice == '4'): 
+  elif(clicked.get() == 'France'): 
+     scrape(FranceURL)
+
+  elif(clicked.get() == 'Austria'): 
    scrape(AustriaURL)
 
-elif(choice == '5'): 
-  scrape(germanyUrl)
+  elif(clicked.get() == 'Germany'): 
+    scrape(germanyUrl)
 
-elif (choice == '6'):
-  scrape(UkURL)
+  elif (clicked.get() == 'England'):
+   scrape(UkURL)
 
-elif (choice == '7'): 
-  scrape(spainURL)
+  elif (clicked.get() == 'Spain'): 
+    scrape(spainURL)
 
-elif (choice == '8'):
-  scrape(portugalURL)
+  elif (clicked.get() == 'Portugal'):
+   scrape(portugalURL)
 
-elif (choice == '9'):
-  scrape(switzerlandUrl)
+  elif (clicked.get() == 'Switzerland'):
+   scrape(switzerlandUrl)
 
-elif (choice == '10'): 
-  scrape(polandURL)
-else: 
- print("Invalid choice")
+  elif (clicked.get() == 'Poland'): 
+    scrape(polandURL)
+  else: 
+    print("Invalid choice")
+
+  
+options = ["Malta", "Italy", "Germany", "France", "Austria", 
+"England", "Spain", "Portugal", "Switzerland","Poland"]
+
+clicked = StringVar()
+clicked.set(options[0])
+
+drop = OptionMenu(second_frame, clicked, *options )
+
+drop.pack()
+
+showButton = Button(second_frame, text="Show Covid Regulations", command=show).pack()
+root.mainloop()
+
